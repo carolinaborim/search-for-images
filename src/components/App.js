dec133a65f13a875452f70b8cb59e3f4b2ba25b6
Carolina Borim <carolina.borim@gmail.com>
2019-05-17 17:35:58 +0200

    Uses .then() and passes a callback to it that prints the results as soon as we get the response from API.

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,17 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID 19bb572e988c921346dd8b93c2954fe316ff10e661d1dac5c39c8564fd5da608"
+        }
+      })
+      .then(response => {
+        console.log(response.data.results);
+      });
   }
 
   render() {
