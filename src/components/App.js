69a02126890e57be6436ccf1f7caf75477cfd2d9
Carolina Borim <carolina.borim@gmail.com>
2019-05-17 17:51:21 +0200

    Refactors function to avoid undefined values.

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -11,12 +13,14 @@ class App extends React.Component {
           "Client-ID 19bb572e988c921346dd8b93c2954fe316ff10e661d1dac5c39c8564fd5da608"
       }
     });
-  }
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onSearchSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images.
       </div>
     );
   }
